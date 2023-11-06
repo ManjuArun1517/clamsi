0f8e5bfc98bc0ef72f8cbfd3c3747121409302d9
ManjuArun1517 <manju.bcompany@gmail.com>
2023-11-06 17:57:33 +0530

    first commit

---
+++ my-project/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="w-[900px]">
+      <h1 className="text-center text-danger">MANJUKRISHNA</h1>
+      <h1 className="text-3xl font-bold underline text-center bg-blue-200">
+      Hello world!
+    </h1>
+    </div>
+  );
+}
+
+export default App;
